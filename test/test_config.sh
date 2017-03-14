d4f7cd52ea6fb19b8e6419d41db35e27afb9aa46
Jan Dolinar <jan.dolinar@firma.seznam.cz>
2017-03-14 21:13:07 +0100

    better configuration format + tests

---
+++ test/test_config.sh
@@ -0,0 +1,61 @@
+#!/bin/bash
+
+# shellcheck source=test/setup.sh
+. "$(dirname "$0")/setup.sh"
+
+plan tests 20
+
+cat > "$TMP/conf" <<EOF
+INTERVAL=4
+COUNT=1234
+PROXY=proxy:3128
+OPTS=("-k")
+
+preset p
+    INTERVAL=8
+    COUNT=4321
+    PROXY=""
+    OPTS=("-opt")
+    FILE="filename"
+    SERVERS=("1.example.com" "2.example.com")
+
+preset q
+    INTERVAL=16
+
+EOF
+
+sed '/main "\$@"/d;' "./httptail" > "$TMP/patched_httptail"
+
+test_variable () {
+    is "${!1}" "$2" "$1 is set correctly"
+}
+
+# shellcheck source=httptail
+. "$TMP/patched_httptail"
+
+set -x
+configure --config "$TMP/conf"
+set +x
+
+test_variable "GLOBAL_INTERVAL" "4"
+test_variable "GLOBAL_COUNT" "1234"
+test_variable "GLOBAL_PROXY" "proxy:3128"
+test_variable "GLOBAL_OPTS[*]" "-k"
+test_variable "GLOBAL_FILE" ""
+test_variable "GLOBAL_SERVERS[*]" ""
+
+test_variable "PRESET_INTERVAL" ""
+test_variable "PRESET_COUNT" ""
+test_variable "PRESET_PROXY" ""
+test_variable "PRESET_OPTS[*]" ""
+test_variable "PRESET_FILE" ""
+test_variable "PRESET_SERVERS[*]" ""
+
+test_variable "INTERVAL" "4"
+test_variable "COUNT" "1234"
+test_variable "PROXY" "proxy:3128"
+test_variable "http_proxy" "proxy:3128"
+test_variable "https_proxy" "proxy:3128"
+test_variable "OPTS[*]" "-k"
+test_variable "FILE" ""
+test_variable "SERVERS[*]" ""
---
+++ test/test_config_preset.sh
@@ -0,0 +1,60 @@
+#!/bin/bash
+
+# shellcheck source=test/setup.sh
+. "$(dirname "$0")/setup.sh"
+
+plan tests 20
+
+cat > "$TMP/conf" <<EOF
+INTERVAL=4
+COUNT=1234
+PROXY=proxy:3128
+OPTS=("-k")
+
+preset p
+    INTERVAL=8
+    COUNT=4321
+    PROXY=""
+    OPTS=("-opt")
+    FILE="filename"
+    SERVERS=("1.example.com" "2.example.com")
+
+preset q
+    INTERVAL=16
+
+EOF
+
+sed '/main "\$@"/d;' "./httptail" > "$TMP/patched_httptail"
+
+test_variable () {
+    is "${!1}" "$2" "$1 is set correctly"
+}
+
+# shellcheck source=httptail
+. "$TMP/patched_httptail"
+set -x
+configure --config "$TMP/conf" --preset p
+set +x
+
+test_variable "GLOBAL_INTERVAL" "4"
+test_variable "GLOBAL_COUNT" "1234"
+test_variable "GLOBAL_PROXY" "proxy:3128"
+test_variable "GLOBAL_OPTS[*]" "-k"
+test_variable "GLOBAL_FILE" ""
+test_variable "GLOBAL_SERVERS[*]" ""
+
+test_variable "PRESET_INTERVAL" "8"
+test_variable "PRESET_COUNT" "4321"
+test_variable "PRESET_PROXY" ""
+test_variable "PRESET_OPTS[*]" "-opt"
+test_variable "PRESET_FILE" "filename"
+test_variable "PRESET_SERVERS[*]" "1.example.com 2.example.com"
+
+test_variable "INTERVAL" "8"
+test_variable "COUNT" "4321"
+test_variable "PROXY" ""
+test_variable "http_proxy" ""
+test_variable "https_proxy" ""
+test_variable "OPTS[*]" "-opt"
+test_variable "FILE" "filename"
+test_variable "SERVERS[*]" "1.example.com 2.example.com"
